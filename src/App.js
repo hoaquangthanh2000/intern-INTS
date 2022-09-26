4f7707724453d1c86910ae6e5b5ae418e15cbb7d
quangthanh <hoaquangthanh2000@gmail.com>
2022-09-26 17:13:45 +0700

    code 26/9

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+function App() { 
+  const storageJob = JSON.parse(localStorage.getItem('jobs'))
+  const [todo,setTodo] = useState('')
+  const [todos,setTodos] = useState(storageJob ?? [])
+  
+  const handleAddTodo = () => {
+    setTodos(prev => {
+      const newJob = [...prev,todo]
+      const jsonJob = JSON.stringify(newJob)
+      localStorage.setItem('jobs', jsonJob)
+      return newJob
+    })
+    setTodo('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:32}}>
+      <input
+      value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add</button>
+
+      <ul>
+        {
+          todos.map((item,index) => (
+            <li key={index}>{item}</li>
+          ))
+
+        }
+        
+      </ul>
     </div>
   );
 }
